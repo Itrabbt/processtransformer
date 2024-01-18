25b51395fe6ffcbfd5a4cb274a23e146d22e23e6
Vera <920989251@qq.com>
2024-01-18 09:41:12 +0800

    sucessfully run

--- next_time.py
+++ next_time.py
@@ -16,7 +16,7 @@ from processtransformer.models import transformer
 
 parser = argparse.ArgumentParser(description="Process Transformer - Next Time Prediction.")
 
-parser.add_argument("--dataset", required=True, type=str, help="dataset name")
+parser.add_argument("--dataset", default="helpdesk", type=str, help="dataset name")
 
 parser.add_argument("--model_dir", default="./models", type=str, help="model directory")
 
